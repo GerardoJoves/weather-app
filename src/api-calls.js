7f68368036a2c70e9b36ac634699c87db5a9eb15
Gerardo Joves <gerardooenrrique@gmail.com>
2022-12-09 21:44:53 -0400

    set up form to fetch weather info based on user input

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,13 +1,14 @@
 import { countryToAlpha2 } from 'country-to-iso';
+import { events } from './events';
 
 const apiKey = '735aaf0c36a7f88b0964d5592785c2c9';
 
-const getLocationsCoords = async (cityName, countryName) => {
+const getLocations = async (cityName, countryName) => {
   const countryISOCode = countryName ? countryToAlpha2(countryName) : null;
 
   const URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}${
     countryISOCode ? ',' + countryISOCode : ''
-  }&limit=5&appid=${apiKey}`;
+  }&appid=${apiKey}`;
 
   const response = await fetch(URL, { mode: 'cors' });
   const locations = await response.json();
@@ -26,10 +27,19 @@ const processData = (data) => {
   const {
     weather: [{ main, description, icon }],
     main: { temp, feels_like, humidity },
+    sys: { country },
     name,
   } = data;
 
-  return { main, description, icon, temp, feels_like, humidity, name };
+  return { main, description, icon, temp, feels_like, humidity, name, country };
 };
 
-export { processData, getCurrentWeather, getLocationsCoords };
+const getWeatherData = async ({ cityName, countryName }) => {
+  let location = await getLocations(cityName, countryName);
+  location = location[0];
+  const coords = { lat: location.lat, lon: location.lon };
+  const curretWeather = await getCurrentWeather(coords);
+  events.emit('weatherDataReady', processData(curretWeather));
+};
+
+events.on('userWeatherRequest', getWeatherData);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import {
-  getLocationsCoords,
-  getCurrentWeather,
-  processData,
-} from './api-calls';
+import { events } from './events';
+import './style.css';
+import './api-calls.js';
 
-getLocationsCoords('london')
-  .then((locations) =>
-    getCurrentWeather({
-      lat: locations[0].lat,
-      lon: locations[0].lon,
-    })
-  )
-  .then((data) => console.log(processData(data)));
+const input = document.querySelector('header input');
+const button = document.querySelector('header .search-btn');
+
+const requestWeather = () => {
+  const location = input.value;
+  if (location === '') return;
+
+  let [cityName, countryName] = location.split(',');
+  cityName = cityName.trim();
+  if (countryName) countryName = countryName.trim();
+
+  events.emit('userWeatherRequest', { cityName, countryName });
+};
+
+button.addEventListener('click', requestWeather);
+
+events.on('weatherDataReady', console.log);
